870ddff6e9c23680c33b680d5cf82d9973a4aa8d
patelha29 <146029257+patelha29@users.noreply.github.com>
2023-10-24 00:51:23 +0000

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,5 @@
+# main.py
+def main():
+  print("Hello UWW")
+if __name__ == "__main__":
+  main()
---
+++ test/test_UWW.py
@@ -0,0 +1,9 @@
+# test_UWW.py
+import main  # Import the main.py module
+
+
+def test_hello_uww(capsys):
+  main_output = "Hello UWW\n"
+  main.main()
+  captured = capsys.readouterr()
+  assert captured.out == main_output
